82e96a008d348a3d5498fc07b13bb0ec4bf9538e
eduardomachadopacheco-lang <eduardo.machado.pacheco@escola.pr.gov.br>
2026-05-26 12:26:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu Site com JS</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <div class="container">
+        <h2>Minha Lista de Tarefas</h2>
+        <div class="input-box">
+            <input type="text" id="taskInput" placeholder="Digite uma nova tarefa...">
+            <button id="addBtn">Adicionar</button>
+        </div>
+        <ul id="taskList"></ul>
+    </div>
+
+    <script src="script.js"></script>
+</body>
+</html>
